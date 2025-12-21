f499d3c0e37e161a8cffa532476ff8908b962695
KoreaMoney <kimdowon1404@gmail.com>
2025-12-22 00:39:17 +0900

    test

--- src/BangarlabDynamicNavigation.tsx
+++ src/BangarlabDynamicNavigation.tsx
@@ -8,8 +8,6 @@ import { NavigationMenu } from "./components/NavigationMenu";
 import { MenuItemData, NavigationState } from "./types/menu.types";
 import { buildMenuTree, toggleMenuExpand, expandAllMenus, getExpandedMenuIds, saveExpandedMenuIds, restoreMenuExpansion, loadExpandedMenuIds, saveActiveMenuId, loadActiveMenuId } from "./utils/menuHelpers";
 
-
-
 import "./ui/BangarlabDynamicNavigation.scss";
 
 export function BangarlabDynamicNavigation(props: BangarlabDynamicNavigationContainerProps): ReactElement {
